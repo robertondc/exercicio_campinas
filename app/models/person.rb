8023a63d184165f4a94ed3c8143b58d0eafb1513
Roberto Nogueira <robertondc@gmail.com>
2012-07-23 13:51:15 -0300

    reorder after destroy

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,10 +1,19 @@
 class Person < ActiveRecord::Base
   attr_accessible :name
-  before_create :give_last_position
+  before_create :put_last_position
+  after_destroy :reorder
   
-  def give_last_position
+  def put_last_position
     last_position = Person.maximum(:position)
     self.position = last_position.nil? ? 0 : last_position + 1
+  end
+  
+  def reorder
+    people = Person.all(:order => 'position,name')
+    people.each_with_index do |person,index|
+      person.position = index
+      person.save
+    end
   end
   
 end
