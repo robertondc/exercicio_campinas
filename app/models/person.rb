1eaebdef672df2ed7c54a870800c8d7f7ff19120
Roberto Nogueira <robertondc@gmail.com>
2012-07-23 05:49:54 -0300

    remove reference to a invalid method

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,14 +1,10 @@
 class Person < ActiveRecord::Base
   attr_accessible :name
   before_create :give_last_position
-  after_destroy :reorder
   
   def give_last_position
     last_position = Person.maximum(:position)
     self.position = last_position.nil? ? 0 : last_position + 1
-  end
-  def reorder
-    
   end
   
 end
